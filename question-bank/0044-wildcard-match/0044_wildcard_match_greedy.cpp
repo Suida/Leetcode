46674ea9ac42dba44ca6bc4ddf8bad6707edf42c
Suida <suidar@foxmail.com>
2020-03-23 19:35:37 +0800

    Modify non-greedy match, add greedy match implemented with dynamic programming

--- question-bank/0044-wildcard-match/0044_wildcard_match_greedy.cpp
+++ question-bank/0044-wildcard-match/0044_wildcard_match_greedy.cpp
@@ -9,7 +9,7 @@
  *  说明:
  *      s可能为空，且只包含从a-z的小写字母。
  *      p可能为空，且只包含从a-z的小写字母，以及字符?和*。
- *      非贪婪匹配。
+ *      贪婪匹配。
  * ----------------------------------
  * 示例 1:
  * 输入:
@@ -34,30 +34,44 @@ using namespace std;
 class Solution {
     public:
         bool isMatch(string s, string p) {
-            int i = 0, j = 0;
-            while (i < s.size() && j < p.size()) {
-                if (p[j] == '?' || s[i] == p[j]) {
-                    ++i;
-                    ++j;
-                    continue;
-                }
-                if (p[j] == '*') {
-                    if (j == p.size()-1) return true;
-                    (p[j+1] == '?' || s[i] == p[j+1])? ++j: ++i;
-                    continue;
-                }
-                return false;
-            }
+            int n = s.size();
+            int m = p.size();
 
-            return i == s.size() && j == p.size();
+            bool** dp = new bool*[n+1];
+            for (int i = 0; i < n+1; ++i)
+                dp[i] = new bool[m+1];
+
+            for (int i = 0; i <= n; ++i)
+                for (int j = 0; j <= m; ++j)
+                    dp[i][j] = false;
+            
+            dp[0][0] = true;
+            for (int j = 1; j <= m; ++j)
+                dp[0][j] = dp[0][j-1] && p[j-1] == '*';
+
+            for (int i = 1; i <= n; ++i)
+                for (int j = 1; j <= m; ++j) {
+                    if (s[i-1] == p[j-1] || p[j-1] == '?')
+                        dp[i][j] = dp[i-1][j-1];
+                    else if (p[j-1] == '*')
+                        dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    else
+                        dp[i][j] = false;
+                }
+
+            return dp[n][m];
         }
 };
 
 int main() {
     Solution s;
     cout << s.isMatch("aaaa", "*") << endl;
+    cout << s.isMatch("a", "a*") << endl;
+    cout << s.isMatch("a", "aa*") << endl;
+    cout << s.isMatch("aaabbbaabaaaaababaabaaabbabbbbbbbbaabababbabbbaaaaba",
+            "a*******b") << endl;
     cout << s.isMatch("aaaa", "a*aaa") << endl;
     cout << s.isMatch("aaa", "a*?a") << endl;
-    cout << s.isMatch("aaaa", "*b") << endl;
+    cout << s.isMatch("aaaa", "*a") << endl;
 }
 
