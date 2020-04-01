68daa7b52368acf39e310cdc7b3dd7303be3c9ab
Suida <suidar@foxmail.com>
2020-04-01 21:30:01 +0800

    DP & dfs

--- question-bank/0140-word-break2/solution.cpp
+++ question-bank/0140-word-break2/solution.cpp
@@ -1,80 +1,50 @@
 #include <vector>
 #include <string>
-#include <set>
-#include <utility>
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
-struct Trie {
-    bool isEnd;
-    Trie** next;
-};
-
 class Solution {
 public:
-    Trie& buildTrie(vector<string> const& wordDict) {
-        Trie* root = new Trie, *tmp;
-        root->isEnd = true;
-        root->next = new Trie*[26] {};
-        for (auto& w: wordDict) {
-            tmp = root;
-            for (auto c: w) {
-                if (tmp->next[c-'a'] == nullptr) {
-                    tmp->next[c-'a'] = new Trie;
-                    tmp->next[c-'a']->isEnd = false;
-                    tmp->next[c-'a']->next = new Trie*[26] {};
-                }
-                tmp = tmp->next[c-'a'];
-            }
-            tmp->isEnd = true;
-        }
-        return *root;
-    }
-
-    bool hasWord(string w, Trie& r) {
-        Trie* tmp = &r;
-        for (auto c: w) {
-            if (tmp->next[c-'a'] == nullptr) return false;
-            tmp = tmp->next[c-'a'];
-        }
-        return tmp->isEnd;
-    }
-
-    bool startsWith(string w, Trie& r) {
-        Trie* tmp = &r;
-        for (auto c: w) {
-            if (tmp->next[c-'a'] == nullptr) return false;
-            tmp = tmp->next[c-'a'];
+    bool starts_with(string s, string p) {
+        for (int i=0; i<p.size(); ++i) {
+            if (i == s.size()) return false;
+            if (s[i] != p[i]) return false;
         }
         return true;
     }
-   
+
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        string lst; int slen;
-        pair<string, int> tu; 
-        auto dp = new vector<decltype(tu)>[s.size()+1] {};
-        dp[0].push_back({"", 0});
-        Trie& words = buildTrie(wordDict);
-        for (int i=1; i<s.size()+1; ++i) {
-            /* dp[i] = vector<pair<string, int>>(); */
-            for (int j=0; j<dp[i-1].size(); ++j) {
-                tu = dp[i-1][j];
-                slen = tu.first.size();
-                lst = tu.first.substr(tu.second, slen-tu.second)+s[i-1];
-                if (hasWord(lst, words)) {
-                    dp[i].push_back({tu.first+s[i-1]+' ', slen+2});
+        int m = s.size(), n = wordDict.size();
+        int** dp = new int*[m+1] {};
+        for (int i=0; i<m+1; ++i) dp[i] = new int[n] {};
+        dp[0][0] = 1;
+        string w, sfx;
+        // 看起来是 O(n*m*m), 实际上是 O(n*m)
+        for (int i=0; i<m; ++i) {
+            for (int j=0; j<n; ++j) {
+                if (dp[i][j] == 0) continue;
+                for (int k=0; k<n; ++k) {
+                    w = wordDict[k];
+                    sfx = s.substr(i);
+                    if (starts_with(sfx, w)) {
+                        dp[i+w.size()][k] = 1;
+                    }
                 }
-                if (startsWith(lst, words)) {
-                    dp[i].push_back({tu.first+s[i-1], tu.second});
-                }
+                j = n;
             }
         }
+        return dfs(dp, m, n, wordDict);
+    }
+
+    vector<string> dfs(int** dp, int i, int n, vector<string> const& words) {
+        if (i == 0) return {""};
         vector<string> res;
-        for (auto tuu: dp[s.size()]) {
-            slen = tuu.first.size();
-            if (tuu.second == slen) {
-                res.push_back(tuu.first.substr(0, tuu.first.size()-1));
+        for (int j=0; j<n; ++j) {
+            if (dp[i][j] == 0) continue;
+            for (auto s: dfs(dp, i-words[j].size(), n, words)) {
+                s = (s == "")? words[j]: s + " " + words[j];
+                res.push_back(s);
             }
         }
         return res;
