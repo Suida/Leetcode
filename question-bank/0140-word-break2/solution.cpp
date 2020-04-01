88a02c69ec22af7c963b866806a860beeb89426d
Suida <suidar@foxmail.com>
2020-04-01 12:11:50 +0800

    Trie and dp

--- question-bank/0140-word-break2/solution.cpp
+++ question-bank/0140-word-break2/solution.cpp
@@ -1,47 +1,82 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <utility>
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
+struct Trie {
+    bool isEnd;
+    Trie** next;
+};
+
 class Solution {
-private:
-    string S;
-    set<string> D;
-    vector<string> res;
 public:
-    void traceback(vector<int>& spc, string& lst, int i) {
-        if (i == S.size()) return;
-        lst += S[i++];
-        traceback(spc, lst, i);
-        if (D.find(lst) != D.end()) {
-            spc.push_back(i-1);
-            string old = lst; lst = "";
-            if (i != S.size()) traceback(spc, lst, i);
-            else res.push_back(buildRes(spc));
-            lst = old;
-            spc.pop_back();
+    Trie& buildTrie(vector<string> const& wordDict) {
+        Trie* root = new Trie, *tmp;
+        root->isEnd = true;
+        root->next = new Trie*[26] {};
+        for (auto& w: wordDict) {
+            tmp = root;
+            for (auto c: w) {
+                if (tmp->next[c-'a'] == nullptr) {
+                    tmp->next[c-'a'] = new Trie;
+                    tmp->next[c-'a']->isEnd = false;
+                    tmp->next[c-'a']->next = new Trie*[26] {};
+                }
+                tmp = tmp->next[c-'a'];
+            }
+            tmp->isEnd = true;
         }
-        lst.pop_back();
+        return *root;
     }
 
-    string buildRes(vector<int> const& spc) {
-        int i = 0;
-        string s = "";
-        for (auto j=1; j<spc.size(); ++j) {
-            s += string(S, i, spc[j]-i+1);
-            s += ' ';
-            i = spc[j]+1;
+    bool hasWord(string w, Trie& r) {
+        Trie* tmp = &r;
+        for (auto c: w) {
+            if (tmp->next[c-'a'] == nullptr) return false;
+            tmp = tmp->next[c-'a'];
         }
-        s = string(s, 0, s.size()-1);
-        return s;
+        return tmp->isEnd;
     }
 
+    bool startsWith(string w, Trie& r) {
+        Trie* tmp = &r;
+        for (auto c: w) {
+            if (tmp->next[c-'a'] == nullptr) return false;
+            tmp = tmp->next[c-'a'];
+        }
+        return true;
+    }
+   
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        S = s; D = set<string>(wordDict.begin(), wordDict.end());
-        vector<int> spc {0}; string lst = "";
-        traceback(spc, lst, 0);
+        string lst; int slen;
+        pair<string, int> tu; 
+        auto dp = new vector<decltype(tu)>[s.size()+1] {};
+        dp[0].push_back({"", 0});
+        Trie& words = buildTrie(wordDict);
+        for (int i=1; i<s.size()+1; ++i) {
+            /* dp[i] = vector<pair<string, int>>(); */
+            for (int j=0; j<dp[i-1].size(); ++j) {
+                tu = dp[i-1][j];
+                slen = tu.first.size();
+                lst = tu.first.substr(tu.second, slen-tu.second)+s[i-1];
+                if (hasWord(lst, words)) {
+                    dp[i].push_back({tu.first+s[i-1]+' ', slen+2});
+                }
+                if (startsWith(lst, words)) {
+                    dp[i].push_back({tu.first+s[i-1], tu.second});
+                }
+            }
+        }
+        vector<string> res;
+        for (auto tuu: dp[s.size()]) {
+            slen = tuu.first.size();
+            if (tuu.second == slen) {
+                res.push_back(tuu.first.substr(0, tuu.first.size()-1));
+            }
+        }
         return res;
     }
 };
